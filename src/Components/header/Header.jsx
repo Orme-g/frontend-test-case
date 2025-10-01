6ffb12eea86cd393796b3129646e383c4ccaf215
Ilya <ilya.grafkin@gmail.com>
2025-10-01 21:23:46 +0300

    Header separated from App file

---
+++ src/Components/header/Header.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setUser } from "../../store/store";
+
+import "./header.css";
+
+const Header = () => {
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.app.user);
+
+    useEffect(() => {
+        setTimeout(() => {
+            dispatch(
+                setUser({
+                    id: 1,
+                    name: "Иван Иванов",
+                    email: "ivan@example.com",
+                })
+            );
+        }, 500);
+    }, [dispatch]);
+
+    return (
+        <header className="header">
+            <h1>🛒 Интернет-магазин</h1>
+            <div className="user-info">
+                {user ? <span>Привет, {user.name}!</span> : <span>Загрузка...</span>}
+            </div>
+        </header>
+    );
+};
+export default Header;
