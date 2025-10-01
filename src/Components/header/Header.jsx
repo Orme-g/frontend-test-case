a3f26d3e8e1aa82adc8e5491d357fa527055649b
Ilya <ilya.grafkin@gmail.com>
2025-10-01 21:48:13 +0300

    Product List separated from App file

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setUser } from "../../store/store";
 
-import "./header.css";
+import "./Header.css";
 
 const Header = () => {
     const dispatch = useDispatch();
